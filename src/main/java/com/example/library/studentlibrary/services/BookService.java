be307f727a2801f78b304f971e84cbe03e163e73
shivambadoni19992 <shivambadoni.39@gmail.com>
2022-12-30 19:51:54 +0530

    last

--- src/main/java/com/example/library/studentlibrary/services/BookService.java
+++ src/main/java/com/example/library/studentlibrary/services/BookService.java
@@ -1,7 +1,9 @@
 package com.example.library.studentlibrary.services;
 
+import com.example.library.studentlibrary.models.Author;
 import com.example.library.studentlibrary.models.Book;
 import com.example.library.studentlibrary.models.Genre;
+import com.example.library.studentlibrary.repositories.AuthorRepository;
 import com.example.library.studentlibrary.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,32 +14,37 @@ import java.util.List;
 @Service
 public class BookService {
 
-
     @Autowired
     BookRepository bookRepository2;
 
-    public void createBook(Book book){
-        bookRepository2.save(book);
+    @Autowired
+    AuthorRepository authorRepository;
+
+    public void createBook(Book book) {
+        int authorId = book.getAuthor().getId();
+        Author author = authorRepository.findById(authorId).get();
+        author.getBooksWritten().add(book);
+        book.setAuthor(author);
+        authorRepository.save(author);
     }
 
-    public List<Book> getBooks(String genre, boolean available, String author){
-        List<Book> temp=bookRepository2.findByAvailability(available);
-        List<Book> books = new ArrayList<>(); 
-        Genre gen=Genre.valueOf(genre);
-        for(Book b:temp){
-            if(author==null){
-                
-                if(b.getGenre().equals(gen)){
+    public List<Book> getBooks(String genre, boolean available, String author) {
+        List<Book> temp = bookRepository2.findByAvailability(available);
+        List<Book> books = new ArrayList<>();
+        Genre gen = Genre.valueOf(genre);
+        for (Book b : temp) {
+            if (author == null) {
+
+                if (b.getGenre().equals(gen)) {
                     books.add(b);
                 }
-            }
-            else{
-                if(b.getGenre().equals(gen)&&b.getAuthor().getName().equalsIgnoreCase(author)){
+            } else {
+                if (b.getGenre().equals(gen) && b.getAuthor().getName().equalsIgnoreCase(author)) {
                     books.add(b);
                 }
             }
         }
-        //find the elements of the list by yourself
+        // find the elements of the list by yourself
         return books;
     }
 }
